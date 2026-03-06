8113c49ded19e16f418b7f6961f82ccd131a3889
harmansinghcse <harman.singh.cse@gmail.com>
2026-03-06 19:23:29 +0530

    GUI is working now

--- main.py
+++ main.py
@@ -1,15 +1,48 @@
+import tkinter as tk
 from weather import fetch_weather
 
-city = input("Enter city: ")
+window = tk.Tk()
+window.title("Weather App")
+window.geometry("300x200")
 
-data = fetch_weather(city)
+city_label = tk.Label(window, text="Enter City")
+city_label.pack()
 
-temp = data["main"]["temp"]
-humidity = data["main"]["humidity"]
-weather = data["weather"][0]["description"]
-wind = data["wind"]["speed"]
+city_entry = tk.Entry(window)
+city_entry.pack()
 
-print(f"Temprature: {temp} C")
-print(f"Weather: {weather}")
-print(f"Humidity: {humidity}%")
-print(f"Windspeed: {wind}m/s")
+temp_label = tk.Label(window, text="Temperature: ")
+temp_label.pack()
+
+weather_label = tk.Label(window, text="Weather: ")
+weather_label.pack()
+
+humidity_label = tk.Label(window, text="Humidity: ")
+humidity_label.pack()
+
+wind_label = tk.Label(window, text="Wind Speed: ")
+wind_label.pack()
+
+def get_weather():
+    city = city_entry.get()
+
+    data = fetch_weather(city)
+
+    if data is None or data["cod"] != 200:
+        temp_label.config(text="City not found")
+        return
+
+    temp = data["main"]["temp"]
+    humidity = data["main"]["humidity"]
+    weather = data["weather"][0]["description"]
+    wind = data["wind"]["speed"]
+
+    temp_label.config(text=f"Temperature: {temp} °C")
+    weather_label.config(text=f"Weather: {weather}")
+    humidity_label.config(text=f"Humidity: {humidity}%")
+    wind_label.config(text=f"Wind Speed: {wind} m/s")
+
+search_button = tk.Button(window, text="Search", command=get_weather)
+search_button.pack()
+
+window.mainloop()
